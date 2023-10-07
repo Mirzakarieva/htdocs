9090fd14032dda991b635f92790fdfb6264a8975
Mirzakarieva <144621217+Mirzakarieva@users.noreply.github.com>
2023-10-07 13:52:50 +0500

    while and for

--- homework8/while21/test.php
+++ homework8/while21/test.php
@@ -1,12 +1,18 @@
 <?php
 $n = $_POST["n"] ;
-$a = 10;
+$a = 0;
 
 do{
 $m= $n % 10 ;
-$a*=10 ;
-echo "$n<br>" ;
-}while($n>=10);
+$n = ($n - $m)/10 ;
 
+if($m%2 == 0)
+    $a++ ;
+}while($n/10 > 1);
+
+if($a > 0)
+    echo "BOR" ;
+else
+    echo "YO'Q" ;
 
 ?>
